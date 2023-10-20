3e843daa28973b276061238df2a45416b75fde0d
oja-bitterlife <bitter.life.club@gmail.com>
2023-10-20 20:44:51 +0900

    UI追加

--- UI_PoseMode.py
+++ UI_PoseMode.py
@@ -1,4 +1,5 @@
 import bpy
+from .pose_mode import ChangeRotationMode
 
 # Main UI
 # ===========================================================================================
@@ -8,7 +9,21 @@ class UI_PoseMode(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "OjaUtil"
-    bl_idname = "OJA_UTIL_POSE"
+    bl_idname = "OJA_UTIL_PT_POSE"
 
     def draw(self, context):
-        context.layout.operator("ojautil.pose.changerotationmode")
+        self.layout.operator("ojautil.pose_changerotationmode")
+
+modules = [
+    ChangeRotationMode,
+]
+
+def register():
+    for module in modules:
+        if hasattr(module, "register"):
+            module.register()
+
+def unregister():
+    for module in modules:
+        if hasattr(module, "unregister"):
+            module.unregister()
