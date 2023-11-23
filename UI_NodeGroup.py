a3cdc3743f67a3e9c2f60054fe1cb808c9ee6fba
oja-bitterlife <bitter.life.club@gmail.com>
2023-11-23 14:56:55 +0900

    ノードグループ追加機能移植

---
+++ UI_NodeGroup.py
@@ -0,0 +1,46 @@
+import bpy
+from . import UI_Setting
+from .node_group import UI_AppendNodes
+
+APPEND_NODE_LABEL = "NodeGroupUtil"
+
+# Main UI
+# ===========================================================================================
+# Compositing Tools Panel
+class OjaUtil_PT_nodegroup_ui(bpy.types.Panel):
+    bl_idname = "OJAUTIL_PT_NODEEDIT_UI"
+    bl_label = APPEND_NODE_LABEL
+    bl_space_type = "NODE_EDITOR"
+    bl_region_type = "UI"
+    bl_category = UI_Setting.UI_CATEGORY
+
+    def draw(self, context):
+        self.layout.operator("ojautil.append_nodegroups")
+
+
+class COMPOSIT_PIL_PT_view_ui(bpy.types.Panel):
+    bl_idname = "OJAUTIL_PT_3DVIEW_UI"
+    bl_label = APPEND_NODE_LABEL
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = UI_Setting.UI_CATEGORY
+
+    def draw(self, context):
+        self.layout.operator("ojautil.append_nodegroups")
+
+
+# register/unregister
+# *****************************************************************************
+modules = [
+    UI_AppendNodes,
+]
+
+def register():
+    for module in modules:
+        if hasattr(module, "register"):
+            module.register()
+
+def unregister():
+    for module in modules:
+        if hasattr(module, "unregister"):
+            module.unregister()
---
+++ node_group/UI_AppendNodes.py
@@ -0,0 +1,60 @@
+import bpy
+import os.path
+
+TARGET_NODE_GROUPS = [
+    # Material
+    "OjaNPR2023.10",
+    "OjaSpec2023.10",
+    # GeometryNodes
+    "OjaGN_Billboard_ZRot",
+    "OjaGN_AutoSmooth",
+    "OjaGN_GostBlur2023.11",
+    # Compositing
+    "OjaCOMPIL_Normal",
+]
+
+APPEND_BUTTON_LABEL = "Append NodeGroups"
+
+
+# Append NodeGroups
+# *****************************************************************************
+class AppendNodeGroups(bpy.types.Operator):
+    bl_idname = "ojautil.append_nodegroups"
+    bl_label = APPEND_BUTTON_LABEL
+
+    def execute(self, context):
+        # すでに全部読み込んであれば何もしない
+        if all([bpy.data.node_groups.get(ng) != None for ng in TARGET_NODE_GROUPS]):
+            return{'FINISHED'}
+
+        # データ転送
+        script_file = os.path.realpath(__file__)
+        resource_file = os.path.join(os.path.dirname(script_file), "resource", "resource.blend")
+
+        with bpy.data.libraries.load(resource_file, link=False, relative=True) as (data_from, data_to):
+            for ng in data_from.node_groups:
+                # 追加対象のみ処理
+                if ng in TARGET_NODE_GROUPS:
+                    if bpy.data.node_groups.get(ng):  # 存在すればなにもしない
+                        continue
+                    else:
+                        data_to.node_groups.append(ng)
+                        print("append:", ng)
+
+        return{'FINISHED'}
+
+
+# register/unregister
+# *****************************************************************************
+classes = [
+    AppendNodeGroups,
+]
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
