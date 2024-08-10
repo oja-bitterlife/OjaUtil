f9cf60f773844266bc4a96065075961d3e075716
oja-bitterlife <bitter.life.club@gmail.com>
2024-08-10 20:48:27 +0900

    バージョン更新

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "oja",
     "description" : "",
     "blender" : (4, 1, 0),
-    "version" : (0, 4, 0),
+    "version" : (0, 5, 1),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
